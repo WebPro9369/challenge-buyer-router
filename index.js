d5c37cf5dd85d0ca5ba393e7a393b0f6f1feb233
Marcel Skoda <marcel.sko@gmx.com>
2017-03-15 12:22:44 -0700

    Add Server and Redis Manipulation functions

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+const port = 3000
+
+let server = require('./lib/server')()
+
+// Start server
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log('Express server listening on %d', port);
+});
